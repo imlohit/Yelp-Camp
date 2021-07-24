4a26e69465e6f67dc92a14e4f75df60a8ef45c06
Lohit Agarwalla <lohit.agarwalla789@gmail.com>
2021-07-24 15:57:27 +0530

    add create and read logic of CRUD

--- app.js
+++ app.js
@@ -13,16 +13,30 @@ db.once('open', function () {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('home');
 })
 
 app.get('/campground', async (req, res) => {
-    const camp = new campGround({ title: 'My terrace', description: 'gaze at stars' });
-    await camp.save();
-    res.send(camp);
+    const campgrounds = await campGround.find({});
+    res.render('campgrounds/index', { campgrounds });
+});
+app.get('/campground/new', (req, res) => {
+    res.render('campgrounds/new');
 })
+app.get('/campground/:id', async (req, res) => {
+    const { id } = req.params;
+    const campgrounds = await campGround.findById({ _id: id });
+    res.render('campgrounds/show', { campgrounds });
+});
+app.post('/campground', async (req, res) => {
+    const campground = new campGround(req.body.campground);
+    await campground.save();
+    res.redirect(`/campground/${campground._id}`);
+})
+
 app.listen(3000, (req, res) => {
     console.log('Listening from port 3000!!!');
 })
