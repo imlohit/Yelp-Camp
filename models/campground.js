f818ee5c668f5895b8ece15511d88ff38b283a72
Lohit Agarwalla <lohit.agarwalla789@gmail.com>
2021-08-09 16:07:14 +0530

    add virtual thumbnail property in campgrounds model

--- models/campground.js
+++ models/campground.js
@@ -3,14 +3,17 @@ const Review = require("./review");
 const User = require("./user");
 const Schema = mongoose.Schema;
 
+const imageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+imageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
 const campgroundSchema = new Schema({
   title: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [imageSchema],
   price: Number,
   description: String,
   location: String,
