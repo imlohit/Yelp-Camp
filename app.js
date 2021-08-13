9cecd854bb42d57e3fb007e12e4369a4498712c9
Lohit Agarwalla <lohit.agarwalla789@gmail.com>
2021-08-13 22:49:23 +0530

    fix error

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const User = require("./models/user");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const MongoDBStore = require("connect-mongo")(session);
-const dbUrl = "mongodb://localhost:27017/yelpCamp" || process.env.DB_URL;
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelpCamp";
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
